b504efb76a86bb8254306abc4bef6767e275c5fc
Igor Amorim <igor.br.santos227@gmail.com>
2021-04-10 11:30:55 -0300

    criando o primeiro modulo

---
+++ modulo_figura_logo.js
@@ -0,0 +1,6 @@
+/* definindo uma variável constante que recebe um objeto js */
+const logo = {
+    uri: 'https://reactnative.dev/img/tiny_logo.png', //endereço da imagem de um logo
+    width: 64,       // largura da figura
+    height: 64      // altura da figura
+  };             // fim da caixa que dos atributos do objeto
